f2e6880de37eeebb3a542e3a9134b37ebaf75061
Dinara Khisametdinova <111443590+dinarasaurae@users.noreply.github.com>
2023-06-22 17:17:22 +0300

    meow

--- explore_page.py
+++ explore_page.py
@@ -5,10 +5,11 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 df = pd.read_csv('mergeddff_app')
-
+df3 = pd.read_csv('df3.csv')
 def show_explore_page():
     st.title('От чего зависит доход специалистов?')
     st.write('''### *по данным исследования зп по европейским странам 2019-2020 ''')
+    st.write(df)
     st.write(':blue[Зависимомть зарплаты от количества лет опыта]')
     fig = px.scatter(
         df, 
@@ -33,7 +34,89 @@ def show_explore_page():
                         )
     st.plotly_chart(fig)
 
+    fig = go.Figure(data=go.Scatter(x=df3['age'], y=df3['net_salary'], mode='markers', marker=dict(color='blue')))
 
 
+    fig.update_layout(
+        title='Сравнение Зарплаты и Возраста',
+        xaxis=dict(title='Возраст'),
+        yaxis=dict(title='Зарплата', range=[0, 250000])
+    )
 
-    
+    st.plotly_chart(fig)
+
+    salary_avg = df.groupby('company_type')['net_salary'].mean().reset_index()
+
+    # Фильтрация нулевых значений
+    salary_avg = salary_avg[salary_avg['company_type'] != 0]
+
+    # Сортировка данных по усредненной зарплате
+    salary_avg = salary_avg.sort_values('net_salary', ascending=False)
+
+    # Создание столбчатой диаграммы
+    fig = go.Figure(data=go.Bar(x=salary_avg['company_type'], y=salary_avg['net_salary'], marker=dict(color='blue')))
+
+    # Настройка макета
+    fig.update_layout(
+        title='Сравнение зарплаты и типа компании',
+        xaxis=dict(title='Тип компании'),
+        yaxis=dict(title='Зарплата')
+    )
+
+    # Показать график с использованием Streamlit
+    st.plotly_chart(fig)
+
+    salary_avg = df.groupby(['seniority_level', 'gender'])['net_salary'].mean().reset_index()
+
+    # Создание столбчатой диаграммы
+    fig = go.Figure()
+
+    # Добавление данных для мужчин
+    fig.add_trace(go.Bar(x=['Junior', 'Middle', 'Senior'], y=salary_avg[salary_avg['gender'] == 'Male']['net_salary'],
+                     name='Male', marker=dict(color='blue')))
+
+    # Добавление данных для женщин
+    fig.add_trace(go.Bar(x=['Junior', 'Middle', 'Senior'], y=salary_avg[salary_avg['gender'] == 'Female']['net_salary'],
+                     name='Female', marker=dict(color='orange')))
+
+    # Настройка макета
+    fig.update_layout(
+        title='Сравнение зарплаты по полу и уровню старшинства',
+        xaxis=dict(title='Уровень старшинства'),
+        yaxis=dict(title='Зарплата')
+    )
+
+    # Показать график с использованием Streamlit
+    st.plotly_chart(fig)
+
+    salary_avg = df.groupby('company_business_sector')['net_salary'].mean().reset_index()
+
+    # Сортировка данных по усредненной зарплате
+    salary_avg = salary_avg.sort_values('net_salary', ascending=False)
+
+    # Создание bar plot
+    fig = go.Figure(data=go.Bar(x=salary_avg['company_business_sector'], y=salary_avg['net_salary'], marker=dict(color='blue')))
+
+    # Настройка макета
+    fig.update_layout(
+        title='Сравнение зарплаты и бизнес сектора компании',
+        xaxis=dict(title='Бизнес сектор'),
+        yaxis=dict(title='Зарплата')
+    )
+
+    # Показать график с использованием Streamlit
+    st.plotly_chart(fig)
+
+    salary_avg = df.groupby('position')['net_salary'].mean().reset_index()
+    salary_avg = salary_avg.sort_values('net_salary', ascending=False)  # Сортировка по усредненной зарплате
+
+    # Создание графика
+    fig = go.Figure(data=go.Bar(x=salary_avg['position'], y=salary_avg['net_salary'], marker=dict(color='blue')))
+
+    # Настройка макета графика
+    fig.update_layout(
+        title='Сравнение усредненной зарплаты по должностям',
+        xaxis=dict(title='Должность'),
+        yaxis=dict(title='Усредненная зарплата')
+    )
+    st.plotly_chart(fig)
--- predict_page_copy.py
+++ predict_page_copy.py
@@ -44,19 +44,68 @@ def show_predict_page():
 
     positions = (
         'QA',
-        'Software Engineer'
+        'Software Engineer',
+        'Frontend Developer',
+        'Designer',
+        'PM',
+        'Engineering manager',
+        'Network Engineer',
+        'Hardware Engineer',
+        'IOS Developer'
     )
 
-    experience = st.slider('опыт', 0.0, 40.0, 0.2)
-    age = st.slider('возраст', 0.0, 60.0, 0.2)
+    company_types = (
+        'Pharma',
+        'E-commerce',
+        'Consulting',
+        'Product',
+        'Fintech',
+        'Media',
+        'Personal Ltd',
+        'Cloud',
+        'FAANG',
+        'Enterprise',
+        'Manufacturing',
+        'Education'
+    )
+
+    cities = (
+        'Kiev',
+        'Oslo',
+        'Berlin',
+        'Saint-Petersburg',
+        'Munich',
+        'Karlsruhe',
+        'Hamburg',
+        'Augsburg ', 
+        'Bremen' ,
+        'Frankfurt',
+        'Regensburg', 
+        'Lisbon', 
+        'Leipzig', 
+        'Stuttgart',
+        'Amsterdam',
+        'Luxembourg',
+        'Bern',
+        'Bonn', 
+        'Köln',
+        'London',
+        'Moscow'
+    )
+
+    company_size = st.slider('размер компании', 10.0, 10000.0, 10.0)
+    experience = st.slider('опыт', 1.0, 40.0, 0.2)
+    age = st.slider('возраст', 16.0, 60.0, 0.2)
     seniority = st.selectbox('уровень', seniority_levels)
     main_lang = st.selectbox('основной язык общения с коллегами', main_langs)
     gender = st.selectbox('гендер', genders)
     position = st.selectbox('позиция', positions)
+    company_type = st.selectbox('тип_компании', company_types)
+    city = st.selectbox('город', cities)
     
 
     flag = st.button('Рассчитать')
     if flag:
-        X = pd.DataFrame(data=[[experience, age, seniority, main_lang, gender, position]], columns=['experience', 'age', 'seniority_level', 'main_language', 'gender', 'position'])
+        X = pd.DataFrame(data=[[company_size, experience, age, seniority, main_lang, gender, position, company_type, city]], columns=['company_size', 'experience', 'age', 'seniority_level', 'main_language', 'gender', 'position', 'company_type', 'city'])
         salary = data.predict(X)
         st.subheader(f'Твоя зарплата €{salary[0]:2f}')
